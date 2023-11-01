b51c21434df03183b09a81f912caa07b736593e6
John Campion Jr <john@brightshore.com>
2023-11-01 02:20:45 -0400

    ci: configure release-it

---
+++ .release-it.cjs
@@ -0,0 +1,26 @@
+/* eslint-disable no-template-curly-in-string */
+// https://github.com/release-it/release-it/blob/main/config/release-it.json
+module.exports = {
+  plugins: {
+    'release-it-changelogen': {
+      excludeAuthors: ['John Campion'],
+    },
+  },
+  git: {
+    commit: true,
+    tag: true,
+    push: true,
+    requireCleanWorkingDir: true,
+    tagName: 'v${version}',
+    commitMessage: 'chore(release): v${version}',
+    tagAnnotation: 'v${version}',
+  },
+  github: {
+    releaseName: 'v${version}',
+    release: true,
+    web: true,
+  },
+  npm: {
+    publish: true,
+  },
+}
